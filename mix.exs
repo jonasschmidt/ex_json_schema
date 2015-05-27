2ed2bb891bfb988525efc7d32dc54fd180ba8420
Jonas Schmidt <jonas.schmidt@gmx.com>
2015-05-27 20:15:47 +0200

    Remove :httpoison application from mix.exs and only start it in the test helper

--- mix.exs
+++ mix.exs
@@ -12,7 +12,7 @@ defmodule ExJsonSchema.Mixfile do
   #
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger, :httpoison]]
+    [applications: [:logger]]
   end
 
   # Dependencies can be Hex packages:
