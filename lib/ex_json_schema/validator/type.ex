138177c344b0fb05c2944be66e7f5a8da6506964
Andrew Summers <oasummerso@gmail.com>
2018-04-22 21:34:54 -0500

    More specific pattern matching.

--- lib/ex_json_schema/validator/type.ex
+++ lib/ex_json_schema/validator/type.ex
@@ -50,12 +50,11 @@ defmodule ExJsonSchema.Validator.Type do
   defp valid?(_, "boolean", data), do: is_boolean(data)
   defp valid?(_, "string", data), do: is_binary(data)
   defp valid?(_, "integer", data) when is_integer(data), do: true
+  defp valid?(4, "integer", _), do: false
 
   defp valid?(version, "integer", data) when version >= 6 do
     is_float(data) and Float.round(data) == data
   end
-
-  defp valid?(_, "integer", _), do: false
 
   defp valid?(version, type, data) when is_list(type) do
     Enum.any?(type, &valid?(version, &1, data))
