c63ef682dd4391ce3d3f999283e0d04d55666ce9
Jonas Schmidt <jonas.schmidt@wooga.net>
2018-06-01 16:36:47 +0200

    Re-enable some tests that were not broken

--- test/json_schema_draft4_test_suite_test.exs
+++ test/json_schema_draft4_test_suite_test.exs
@@ -12,10 +12,8 @@ defmodule ExJsonSchema.JsonSchemaDraft4TestSuiteTest do
       "base URI change - change folder: number is valid",
       "base URI change - change folder: string is invalid",
       "base URI change: base URI change ref valid",
-      "remote ref, containing refs itself: remote ref valid",
       "root ref in remote ref: null is valid",
       "root ref in remote ref: object is invalid",
-      "root ref in remote ref: string is valid",
-      "valid definition: valid definition schema"
+      "root ref in remote ref: string is valid"
     ]
 end
