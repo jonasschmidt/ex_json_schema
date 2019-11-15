714afff1d4953bd860d10d27eb1c0455d320563b
Jonas Schmidt <jonas.schmidt@wooga.net>
2019-11-15 14:38:10 +0100

    Ensure that tests are run when dialyzer is enabled

--- mix.exs
+++ mix.exs
@@ -35,7 +35,7 @@ defmodule ExJsonSchema.Mixfile do
     [
       {:httpoison, "~> 0.8", only: :test},
       {:poison, "~> 1.5", only: :test},
-      {:excoveralls, "~> 0.4", only: :test},
+      {:excoveralls, "~> 0.10", only: :test},
       {:mix_test_watch, "~> 0.2.6", only: [:dev, :test]},
       {:ex_doc, ">= 0.0.0", only: :dev},
       {:dialyxir, "~> 0.5", only: [:dev, :test], runtime: false}
