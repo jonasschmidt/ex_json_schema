81c8b031a9fb9317c514f03a874f74819bb0cf9e
Jonas Schmidt <jonas.schmidt@wooga.net>
2021-06-10 11:48:09 +0200

    Bump version to 0.8.1

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule ExJsonSchema.Mixfile do
   use Mix.Project
 
   @source_url "https://github.com/jonasschmidt/ex_json_schema"
-  @version "0.8.0-rc1"
+  @version "0.8.1"
 
   def project do
     [
