a101983b11f17fb75f3a3fd3cad04393087ec6fa
Jonas Schmidt <jonas.schmidt@wooga.net>
2022-09-12 11:36:04 +0200

    Bump version to 0.9.2

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ Add the project to your Mix dependencies in `mix.exs`:
 ```elixir
 defp deps do
   [
-    {:ex_json_schema, "~> 0.9.1"}
+    {:ex_json_schema, "~> 0.9.2"}
   ]
 end
 ```
--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule ExJsonSchema.Mixfile do
   use Mix.Project
 
   @source_url "https://github.com/jonasschmidt/ex_json_schema"
-  @version "0.9.1"
+  @version "0.9.2"
 
   def project do
     [
